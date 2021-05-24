6f9ebbbf08ce38c6f296db82b0a28ebe6c8c0aab
bholebaba0135 <anmol.gupta2357@gmail.com>
2021-05-24 15:12:05 +0530

    first commit

---
+++ events.js
@@ -0,0 +1,19 @@
+var right=document.getElementById("right");
+var left=document.getElementById("left");
+var rec_list = document.getElementsByClassName("rec_list");
+right.addEventListener("click",()=>{
+  var pos = rec_list[0].offsetLeft;
+  if(pos >-2500){
+  var moveleft= pos-370;
+  var moveleft = moveleft.toString();
+  document.getElementById("rec_list").style.left= moveleft+"px";
+}
+});
+left.addEventListener("click",()=>{
+  const pos = rec_list[0].offsetLeft;
+  if(pos!=0){
+    var moveleft= pos+370;
+    var moveleft = moveleft.toString();
+    document.getElementById("rec_list").style.left= moveleft+"px";
+  }
+})
